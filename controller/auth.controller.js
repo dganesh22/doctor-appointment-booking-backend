bdf3cd6c678d92a9e148246c9f7a9a26552399b1
dganesh22 <dganesh2022@gmail.com>
2023-08-02 12:06:58 +0530

    updated login and register

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,7 @@
 const  { StatusCodes }  = require('http-status-codes')
 const User = require('../model/user.model')
+const bcrypt = require('bcryptjs')
+const createAccessToken = require('../util/token')
 
 // register 
 const register = async (req,res) => {
@@ -13,8 +15,15 @@ const register = async (req,res) => {
         const extMobile = await User.findOne({ mobile: req.body.mobile })
                 if(extMobile)
                     return res.status(StatusCodes.BAD_REQUEST).json({ msg: `${req.body.mobile} already exists.`})
+               
+            const encPass = await bcrypt.hash(req.body.password,10) // password hash (10 bit)
 
-        const newUser = await User.create(req.body)
+        const newUser = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            mobile: req.body.mobile,
+            password: encPass
+        })
 
             return res.status(StatusCodes.OK).json({ msg: "User registered successfully", user: newUser })
 
@@ -27,7 +36,28 @@ const register = async (req,res) => {
 // login
 const login = async (req,res) => {
     try {
-        res.status(StatusCodes.OK).json({ msg: "login"})
+         const user = req.body
+
+         // user email exists or not
+         const extUser = await User.findOne({ email: user.email })
+            if(!extUser)
+                return res.status(StatusCodes.NOT_FOUND).json({ msg: `User ${user.email} doesn't exists.`})
+
+        // verify the password
+        const isMatch = await bcrypt.compare(user.password,extUser.password)
+            if(!isMatch)
+                return res.status(StatusCodes.UNAUTHORIZED).json({ msg: `Passwords aren't matched`})
+        
+        // check user is active or not
+        if(!extUser.isActive)
+            return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Sorry, Your account is blocked"})
+
+
+        // generate token
+        const accessToken = createAccessToken({ _id: extUser._id })
+
+
+        res.status(StatusCodes.OK).json({  msg: "Login Successful", accessToken })
     } catch (err) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.message })
     }
